19ff56f6f72d80de41ed3fe3c22509d482d1caad
Stranger3333 <1519272170@qq.com>
2022-02-02 21:01:44 -0600

    feb2 version

--- MP0/logger.py
+++ MP0/logger.py
@@ -1,14 +1,12 @@
-from concurrent.futures import thread
-from distutils.command.build_scripts import first_line_re
 import socket
 import sys
 import time
 import os
 from _thread import *
 import threading
+import csv
 
-from cv2 import add
-
+# HOST = '172.22.158.15'
 HOST = '127.0.0.1'
 PORT = int(sys.argv[1])
 Threadnum = 0
@@ -20,18 +18,40 @@ if len(sys.argv) > 2:
 def nodenum():
     print('abc')
 class server:
-    
+    def storedata(self,data,width):
+        recv_time = time.time()
+        gen_time = float(data.split()[0])
+        delay = recv_time-gen_time
+        newline = data+' '+str(delay)+' '+str(width)
+        print(newline)
+        return newline.split()
+
     def multinode(self,connection, addr,Threadnum):
         
         node_connected = connection.recv(1024).decode('utf8').split()
-        print(node_connected[0],'-','node'+str(Threadnum),'connected')
+        f = open(node_connected[1]+'.csv','w')
+        writer = csv.writer(f)
+        # writer.writerow(node_connected)
+        # print(node_connected)
+        # node_connected = node_connected.split()
+        print(time.time(),'-',node_connected[1],'connected')
         with connection:
-            print('Connected by', addr)
+            # print('Connected by', addr)
+            
             while True:
                 # print('waiting for nodes connection....')
-                data = connection.recv(1024).decode("utf-8").split()
-                print(data[0], 'node'+str(Threadnum),data[1])
+                data = connection.recv(1024).decode("utf-8")
+                if not data: 
+                    print(time.time(),'-',node_connected[1],'disconnected')
+                    break
+                width = len(data)
+                line = self.storedata(data,width)
+                # f.write(line)
+                writer.writerow(line)
+                data = data.split()
+                print(data[0],data[1],data[2])
         connection.close()
+        f.close()
         
     def server_conn(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- MP0/node.py
+++ MP0/node.py
@@ -2,9 +2,13 @@ import socket
 import sys
 import time
 import os
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
-print(HOST,PORT)
+HOST = sys.argv[2]
+PORT = int(sys.argv[3])
+nodename = sys.argv[1]
+# print(HOST,PORT)
+if len(sys.argv) != 4:
+    print('Incorrect input arguments')
+    sys.exit(0)
 class Node:
     def client(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,7 +18,11 @@ class Node:
             # print('abc')
             for line in sys.stdin:
                 print(line)
-                sock.sendall(line.encode())
+                data = [line.split()[0],nodename,line.split()[1]]
+                # if not data: break
+                data = str(' '.join(data))
+                sock.sendall(data.encode())
+            sock.close()
         
                 
 
